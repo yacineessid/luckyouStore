5df27ca7b47c39aafe07a2aea5aa917674ff8acb
yacineessid <yacine.essid@gmail.com>
2021-11-29 16:33:33 +0100

    last commit

--- backend/server.js
+++ backend/server.js
@@ -38,23 +38,22 @@ const __dirname = path.resolve()
 
 app.use('/uploads' , express.static(path.join(__dirname ,'/uploads')))
 
+if (process.env.NODE_ENV === "production") {
+  app.use("/", express.static("frontend/build"));
 
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '/frontend/build')))
-
-  app.get('*', (req, res) =>
-    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
-  )
+  app.get("*", (req, res) => {
+      res.sendFile(path.resolve(__dirname, "frontend/build/index.html"));
+  });
 } else {
-  app.get('/', (req, res) => {
-    res.send('API is running....')
-  })
+  app.get("/", (req, res) => {
+      res.send("API is running....");
+  });
 }
 
-app.use(notFound)
-app.use(errorHandler)
+app.use(notFound);
+app.use(errorHandler);
 
-
-const PORT = process.env.PORT || 5000 
-app.listen(5000 , console.log
-     (`Server is running in ${process.env.NODE_ENV}mode on port ${PORT}`.yellow.bold))
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+  console.log(`APP listening at ${port}`);
+});
